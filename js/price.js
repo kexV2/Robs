fe3392047b95fc597f1bcac5fdeb018e89198946
kexV2 <147058686+kexV2@users.noreply.github.com>
2024-03-06 15:02:53 +0000

    Delete js directory

--- js/price.js
+++
@@ -1,7 +0,0 @@
-function showTotalWithShipping() {
-  // add 10 euros for shipping
-  var totalWithShipping = totalCost + 10;
-
-  // create a pop-up message
-  alert("Your total amount plus a base 10 euro shipping is: " + totalWithShipping.toFixed(0) + "€");
-}
